80cb10a415500c341d3bcd07a3d51136931fa8b1
Jack Bottlaender <jack.bottlaender@effem.com>
2020-07-01 16:05:47 +0200

    Finish clean_data function
    This commit adds code to the clean_data function to format the dataframe resulting from load_data.

--- data/process_data.py
+++ data/process_data.py
@@ -27,7 +27,50 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 def clean_data(df):
-    pass
+    '''
+    This function takes the dataframe containing the combined message and category data, encodes the categories as binary columns, and removes duplicates and misclassified rows.
+
+    INPUT:
+    df - dataframe combining the messages and the raw data for their associated categories
+
+    OUTPUT:
+    clean_df - the dataframe with the categories encoded as binary columns, and duplicates and misclassified rows removed
+    '''
+
+    # Create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(pat=';', expand=True)
+    
+    # Select the first row of the categories dataframe
+    row = categories.iloc[0, :]
+    
+    # Use this row to extract a list of new column names for categories
+    category_colnames = [x[0:-2:1] for x in row]
+    
+    # Use this list to rename the columns of the categories dataframe
+    categories.columns = category_colnames
+    
+    # Convert category values to just 0s and 1s
+    for column in categories.columns:
+        
+        # Set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str.slice(start=-1)
+    
+        # Convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    # Drop the original categories column from df
+    df.drop(columns=['categories'], inplace=True)
+    
+    # Concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # Drop the rows where related values equal 2
+    df = df.drop(index=df[df['related'] == 2].index)
+
+    # Drop duplicates
+    clean_df = df.drop(index=df[df.duplicated()].index)
+
+    return clean_df
 
 
 def save_data(df, database_filename):
