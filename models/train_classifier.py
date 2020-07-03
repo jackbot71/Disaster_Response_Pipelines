e3a8d6dfe46a26dafd20df086212e5b41069019a
Jack Bottlaender <jack.bottlaender@effem.com>
2020-07-03 17:39:30 +0200

    Modify main() to include table name argument

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -171,10 +171,10 @@ def save_model(model, model_filepath):
 
 
 def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
+    if len(sys.argv) == 4:
+        database_filepath, database_tablename, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y = load_data(database_filepath)
+        X, Y = load_data(database_filepath, database_tablename)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
@@ -193,9 +193,9 @@ def main():
 
     else:
         print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'as the first argument, the table name as the second and the filepath of the pickle file to '\
+              'save the model to as the third argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db msg_df classifier.pkl')
 
 
 if __name__ == '__main__':
